cd2f01616bd6154c054abdc1f3199d58696be781
cudinh6941 <khadinh41@gmail.com>
2025-05-04 15:36:46 +0700

    Initial Commit

---
+++ Nestify/NestifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestifyApp.swift
+//  Nestify
+//
+//  Created by pham kha dinh on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct NestifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
